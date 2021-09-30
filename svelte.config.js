97d113f6a76a2451a31dd2c0ab378dc5cade3d75
borris wiria <borris.wiria@gmail.com>
2021-09-30 16:32:09 +0700

    fix build

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,17 @@
 import preprocess from 'svelte-preprocess';
 
-import netlify from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	// preprocess: preprocess(),
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: netlify(),
+		adapter: adapter(),
 		vite: {
 			optimizeDeps: {
 				exclude: ['@urql/svelte']
